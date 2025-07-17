f3d5069289ed5ce05fe7abf34ce755562ecc855a
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2025-07-17 19:53:35 +0530

    Initial commit

---
+++ apps/acadura-erp/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'acadura-erp',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/acadura-erp',
+};
